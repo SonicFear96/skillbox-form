a13b63eb94714f5f6ebe71264a893e192b8be074
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2023-08-01 21:32:07 +0300

    feature: payment cards

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,13 @@
       <vButton> Отправить </vButton>
     </div>
     <div class="ui">
-      <vButton :disabled="true"> Отправить </vButton>
+      <vButton> Отправить </vButton>
     </div>
     <div class="ui">
-      <vField :error="true">label</vField>
+      <vField :value="''">label</vField>
+    </div>
+    <div class="ui">
+      <cardPayment :data="payment[3]" />
     </div>
   </div>
 </template>
@@ -19,11 +22,47 @@
 <script>
 import vButton from "@/components/common/button";
 import vField from "@/components/common/field";
+import cardPayment from "@/components/cards/payment";
 export default {
   name: "App",
   components: {
     vButton,
     vField,
+    cardPayment,
+  },
+  data() {
+    return {
+      payment: [
+        {
+          id: 1,
+          alias: "consultation",
+          text: "Оставить заявку<br>на консультацию",
+          images: [
+            { id: 1, title: "Visa", image: "visa" },
+            { id: 2, title: "Мир", image: "mir" },
+            { id: 3, title: "Mastercard", image: "mastercard" },
+          ],
+        },
+        {
+          id: 2,
+          alias: "sale",
+          text: "Оплатить всю сумму со скидкой 7000 ₽",
+          images: [],
+        },
+        {
+          id: 3,
+          alias: "sberbank",
+          text: "В рассрочку в Покупай со Сбером",
+          images: [{ id: 1, title: "Сбербанк", image: "sberbank" }],
+        },
+        {
+          id: 4,
+          alias: "tinkoff",
+          text: "В рассрочку<br>в Тинькофф",
+          images: [{ id: 1, title: "Тинькофф", image: "tinkoff" }],
+        },
+      ],
+    };
   },
 };
 </script>
--- src/components/cards/payment/index.vue
+++ src/components/cards/payment/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="card-payment"></div>
+  <article class="card-payment" :class="{ 'card-payment--active': isActive }">
+    <span class="card-payment__text" v-html="data.text"></span>
+    <div class="card-payment__icons-list">
+      <img
+        v-for="item in data.images"
+        class="card-payment__icons-item"
+        :src="getImage(item)"
+        :alt="item.title"
+        :key="item.id"
+      />
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
   name: "card-payment-component",
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+    isActive: Boolean,
+  },
+  methods: {
+    getImage(data) {
+      return require(`@/../static/icons/${data.image}.svg`);
+    },
+  },
 };
 </script>
 
